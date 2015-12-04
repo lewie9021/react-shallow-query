4152798ab39b88dfd0e680d3290b0b908df7ff76
Lewis Barnes <lb.barnes@live.co.uk>
2015-12-04 23:24:40 +0000

    Added specs to cover most of the basic query functionality. - Replaced the TestComponent with Comments and Comment to make testing easier.

--- specs/index.spec.js
+++ specs/index.spec.js
@@ -3,16 +3,32 @@ import React from "react";
 import { renderComponent } from "./helpers";
 import $ from "../src";
 
-function TestComponent({name}) {
+function Comment({author, message}) {
     return (
-        <span>Hello {name}</span>
+        <li className="comment">
+            <span className="author">
+                {author}
+            </span>
+            <span id="message">
+                {message}
+            </span>
+        </li>
+    );
+}
+
+function Comments() {
+    return (
+        <ul>
+            <Comment author="John Smith" message="Hello World" />
+            <li>Some other item</li>
+        </ul>
     );
 }
 
 describe("React Shallow Query", function() {
 
     it("should throw if 'query' isn't a string", function() {
-        const {output} = renderComponent(TestComponent);
+        const {output} = renderComponent(Comment);
         const invalidParameters = [true, false, undefined, 5, {}, [], null];
         const error = "You must provide a query.";
         
@@ -21,6 +37,45 @@ describe("React Shallow Query", function() {
         });
 
         expect(() => $(output, "test")).not.to.throw();
+    });
+
+    it("should return an array of results", function() {
+        const {output} = renderComponent(Comment);
+        const results = $(output, "test");
+
+        expect(Array.isArray(results)).to.eq(true);
+    });
+
+    it("should select elements by class name using a period", function() {
+        const {output} = renderComponent(Comment);
+        const results = $(output, ".author");
+
+        expect(results.length).to.be.at.least(1);
+        results.forEach((result) => expect(result.props.className).to.contain("author"));
+    });
+
+    it("should select elements by id using a hash", function() {
+        const {output} = renderComponent(Comment);
+        const results = $(output, "#message");
+
+        expect(results.length).to.be.at.least(1);
+        results.forEach((result) => expect(result.props.id).to.eq("message"));
+    });
+
+    it("should provide support for selecting components by name", function() {
+        const {output} = renderComponent(Comments);
+        const results = $(output, "Comment");
+
+        expect(results.length).to.be.at.least(1);
+        results.forEach((result) => expect(result.type.name).to.eq("Comment"));
+    });
+
+    it("should allow selecting of elements by tag", function() {
+        const {output} = renderComponent(Comments);
+        const results = $(output, "li");
+
+        expect(results.length).to.be.at.least(1);
+        results.forEach((result) => expect(result.type).to.eq("li"));
     });
     
 });
